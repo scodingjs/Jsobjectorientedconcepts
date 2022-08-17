80d22883f044546513213c5eefbbdcd616cdc7e5
Sangeetha.K <kppkmsanu@gmail.com>
2022-08-16 23:12:32 -0600

    primitive

---
+++ types.js
@@ -0,0 +1,21 @@
+//Js is a complete Object oriented programming language which is flexible thru which is helps us create PAckage and classes.
+// Everything functions  are all objects
+// Primitive Types - Simple Data
+// Reference Types - Objects in memory
+
+//Primitive Types - Number, String, Boolean, Null, Undeifned
+//Reference Types - 
+
+//Primtive Types:
+let studentName="Durga"
+let courseFee = 11000
+let isValid =  true;
+let x=undefined;
+let y = null;
+let z;
+console.log(studentName,typeof(studentName))
+console.log(courseFee,typeof(courseFee))
+console.log(isValid,typeof(isValid))
+console.log(x,typeof(x))
+console.log(y,typeof(y),y===null)
+console.log(z,typeof(z))
